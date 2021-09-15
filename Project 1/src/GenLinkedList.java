f241bf55703d43a778c6619a18919b3aaecf20f7
Varun-Poondi <varunmukundan@gmail.com>
2021-09-15 15:14:12 -0500

    Updated LinkedList Logic, added all deletion functionalities, testing required

--- Project 1/src/GenLinkedList.java
+++ Project 1/src/GenLinkedList.java
@@ -70,20 +70,72 @@ public class GenLinkedList<T extends Comparable<T>>{
         }
     }
     public void set(int position, T payload){
-        
+        if (head != null) {
+            if (position < getSize() && position >= 0) {
+                int counter = 0;
+                Node<T> currentNode = head;
+                while (currentNode.getNext() != null && counter != position) {
+                    counter++;
+                    currentNode = currentNode.getNext();
+                }
+                currentNode.setPayload(payload);
+            }else{
+                System.out.println("Required position is out of bounds");
+            }
+        }
     }
     public T get(int position){
+        if(head != null) {
+            Node<T> currentNode = getNodeAtPosition(position);
+            if(currentNode != null){
+                return currentNode.getPayload();
+            }
+        }
         return null;
     }
+    public Node<T> getNodeAtPosition(int pos){
+        if(head != null){
+            int counter = 0;
+            Node<T> currentNode = head;
+            while(currentNode.getNext() != null && counter != pos){
+                counter ++;
+                currentNode = currentNode.getNext();
+            }
+            return currentNode;
+        }
+        return null;
+    }
+    public void setNodeAtPosition(int pos, T payload){
+        if(head != null){
+            int counter = 0;
+            Node<T> currentNode = head;
+            while(currentNode.getNext() != null && counter != pos){
+                counter ++;
+                currentNode = currentNode.getNext();
+            }
+            currentNode.setPayload(payload);
+        }
+    }
     public void swap(int pos1, int pos2){
-        
+        if(head != null){
+            if(getSize() > 0 && pos1 <= getSize() && pos1 >= 0 && pos2 <= getSize() && pos2 >= 0){
+                setNodeAtPosition(pos1, getNodeAtPosition(pos1).getPayload());
+                setNodeAtPosition(pos2, getNodeAtPosition(pos2).getPayload());
+            }else if(getSize() == 0) {
+                System.out.println("Linked List is Empty");
+            }else{
+                System.out.println("Position 1 or Position 2 is out of bounds\n" +
+                        "Must be between 0 and " + getSize());
+            }
+        }
     }
     public void shift(int shiftVal){
-        
+        //??
     }
     public void print(){
         System.out.print("Current Linked List: ");
         printList(head);
+        System.out.println();
     }
     public void printList(Node<T> node){
         if(node != null){
@@ -91,12 +143,61 @@ public class GenLinkedList<T extends Comparable<T>>{
             printList(node.getNext());
         }
     }
+    public int getSize(){
+        if(head != null) {
+            int size = 0;
+            Node<T> currentNode = head;
+            while(currentNode.getNext() != null){
+                currentNode = currentNode.getNext();
+                size ++;
+            }
+            return size;
+        }
+        return 0;
+    }
+
     /*********************************************************/
     public void removeMatching(T payload){
-        
+         if(head != null){
+             Node<T> currentNode = head;
+             Node<T> previousNode = null;
+             while(currentNode.getNext() != null){
+                 if(currentNode.getPayload() == payload){
+                     if(currentNode == head){
+                         head = head.getNext();
+                     }else if(previousNode != null){
+                         previousNode.setNext(currentNode.getNext());
+                     }
+                 }else{
+                     previousNode = currentNode;
+                 }
+                 currentNode = currentNode.getNext();
+             }
+         }
     }
+    
     public void erase(int position, int limit){
-        
+        if(head != null){
+            if(getSize() > 0 && position <= getSize() && position >= 0 && limit <= getSize() && limit >= 0){
+                if(position < limit) {
+                    Node<T> currentNode = getNodeAtPosition(position); //pointing to the same address
+                    Node<T> previousNode = null;
+                    int counter = position;
+                    while (currentNode.getNext() != null && counter != limit) {
+                        if(currentNode == head){
+                            head = head.getNext();
+                        }else if(previousNode != null){
+                            previousNode.setNext(currentNode.getNext());
+                        }
+                        previousNode = currentNode;
+                        currentNode = currentNode.getNext();
+                        counter ++;
+                    }
+                }else{
+                    System.out.println("Your position value cannot be greater than limit");
+                }
+            }
+        }
     }
     public void insertList(List<T> list){
         
--- Project 1/src/Main.java
+++ Project 1/src/Main.java
@@ -3,7 +3,13 @@ public class Main {
         GenLinkedList<Integer> test = new GenLinkedList<>();
         test.addEnd(0);
         test.addEnd(1);
-        test.addFront(10);
+        test.addEnd(10);
+        test.addEnd(15);
+        test.addEnd(44);
+        test.addFront(72);
+        test.addFront(88);
+        test.print();
+        test.set(5,3340);
         test.print();
     }
 }
