7604d965af0dc0788a9090b53c9ed2cc9916f3ec
Varun-Poondi <varunmukundan@gmail.com>
2021-11-22 22:14:29 -0600

    update

--- Project 4/src/Main.java
+++ Project 4/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
+//import java.util.Scanner;
+//import java.util.StringJoiner;
+//
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       int length;
+       int width;
+       Scanner getInfo = new Scanner(System.in);
+
+        System.out.print("Enter Length: ");
+        length = getInfo.nextInt();
+        System.out.print("Enter Width: ");
+        width = getInfo.nextInt();
+
+        Maze maze = new Maze(length, width);
+        maze.printMaze();
+        System.out.println();
+        maze.createRandomMaze();
+        maze.printMaze();
+
     }
+
 }
+    
+    
+    
+    
+
